cdc4a65b3824e28dcf1203826ca690cd446e4284
Alexander Silyaev <silyaev@gmail.com>
2018-07-21 09:01:31 +0200

    removed not used field

--- src/main/java/it/alex/transfer/config/AppContext.java
+++ src/main/java/it/alex/transfer/config/AppContext.java
@@ -1,7 +1,6 @@
 package it.alex.transfer.config;
 
 import akka.actor.ActorSystem;
-import akka.stream.ActorMaterializer;
 import com.typesafe.config.Config;
 import it.alex.transfer.service.AccountInfoService;
 import it.alex.transfer.service.ApiService;
@@ -13,7 +12,6 @@ import lombok.Data;
 @Builder
 public class AppContext {
     private ActorSystem system;
-    private ActorMaterializer materializer;
     private Config applicationConfig;
     private TransferService transferService;
     private ApiService apiService;
