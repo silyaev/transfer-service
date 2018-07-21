2cab896b0333353c556edd2f86347c8507b5d8ba
Alexander Silyaev <silyaev@gmail.com>
2018-07-21 11:03:07 +0200

    Basic application implementation

--- src/main/java/it/alex/transfer/TransferApp.java
+++ src/main/java/it/alex/transfer/TransferApp.java
@@ -1,17 +1,82 @@
 package it.alex.transfer;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.server.Route;
+import com.typesafe.config.Config;
+import it.alex.transfer.config.AppContext;
+import it.alex.transfer.config.HttpConfig;
+import it.alex.transfer.config.PersistenceConfig;
+import it.alex.transfer.http.HttpRouter;
+import it.alex.transfer.http.HttpServer;
+import it.alex.transfer.service.AccountInfoService;
+import it.alex.transfer.service.TransferService;
+import it.alex.transfer.service.impl.AccountInfoServiceImpl;
+import it.alex.transfer.service.impl.ApiServiceImpl;
+import it.alex.transfer.service.impl.TransferServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public class TransferApp {
+
+    private static final int OK_CODE = 0;
+    private static final int ERROR_CODE = -1;
+    private ActorSystem system;
 
     public static void main(String[] args) {
         System.exit(new TransferApp().run(args));
     }
 
-    private int run(String[] args) {
+    public int run(String[] args) {
         log.info("Simple money transfer service is starting and use command line arguments {}", (Object) args);
-        return 0;
+
+        try {
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                log.info("Close by ShutdownHook.");
+                stop();
+            }));
+            start();
+
+        } catch (Exception ex) {
+            log.error("Application error ", ex);
+            return ERROR_CODE;
+        }
+
+        return OK_CODE;
     }
+
+    public void start() throws ExecutionException, InterruptedException {
+
+        system = ActorSystem.create("routes");
+
+        final Config applicationConfig = system.settings().config().getConfig("application");
+        final HttpConfig httpConfig = HttpConfig.newInstance(applicationConfig);
+        final SessionFactory sessionFactory = PersistenceConfig.newInstance(applicationConfig).getSessionFactory();
+        final AccountInfoService accountInfoService = AccountInfoServiceImpl.newInstance(sessionFactory);
+        final TransferService transferService = TransferServiceImpl.newInstance(sessionFactory);
+
+        final AppContext context = AppContext.builder()
+                .applicationConfig(applicationConfig)
+                .system(system)
+                .accountInfoService(accountInfoService)
+                .transferService(transferService)
+                .build();
+
+        ApiServiceImpl.newInstance(context);
+
+        final Route route = HttpRouter.newInstance(context).createRoute();
+        final HttpServer httpServer = HttpServer.newInstance(context, httpConfig, route);
+        httpServer.start();
+
+    }
+
+    public void stop() {
+        log.info("Start application shutdown");
+        system.terminate();
+        log.info("System is stopped");
+    }
+
 
 }
--- src/main/java/it/alex/transfer/http/HttpServer.java
+++ src/main/java/it/alex/transfer/http/HttpServer.java
@@ -1,55 +1,32 @@
 package it.alex.transfer.http;
 
-import akka.NotUsed;
-import akka.actor.ActorSystem;
-import akka.http.javadsl.ConnectHttp;
-import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.HttpApp;
 import akka.http.javadsl.server.Route;
-import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
 import it.alex.transfer.config.AppContext;
 import it.alex.transfer.config.HttpConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @RequiredArgsConstructor
-public class HttpServer {
+public class HttpServer extends HttpApp {
     private final AppContext context;
     private final HttpConfig config;
     private final Route route;
-    private volatile CompletionStage<ServerBinding> binding;
 
     public static HttpServer newInstance(final AppContext context, final HttpConfig config, final Route route) {
-        final HttpServer httpServer = new HttpServer(context, config, route);
-        httpServer.start();
-        return httpServer;
+        return new HttpServer(context, config, route);
     }
 
-    public void stop() {
-        log.info("Stopping Http Server");
-        binding.thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> log.info(" Http Server is stopped {}", unbound));
-    }
-
-    private void start() {
-        log.info("Starting Http server use {}", this.config.getHttpConfig());
-
-        final ActorSystem system = context.getSystem();
-        final ActorMaterializer materializer = context.getMaterializer();
-
-        final Http http = Http.get(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = route.flow(system, materializer);
-
-        binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost(config.getHost(), config.getPort()), materializer);
+    public void start() throws ExecutionException, InterruptedException {
+        this.startServer(config.getHost(), config.getPort(), context.getSystem());
 
     }
 
-
+    @Override
+    protected Route routes() {
+        return route;
+    }
 }
